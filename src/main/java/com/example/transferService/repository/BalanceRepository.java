e857ca4852c1e01d8c2e2bba563f506ded20ba64
Olagunju Babatunde Yunus <thundex001@gmail.com>
2018-11-27 20:19:44 +0000

    Add comments

--- src/main/java/com/example/transferService/repository/BalanceRepository.java
+++ src/main/java/com/example/transferService/repository/BalanceRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
-
+/**
+ * Repository that interacts with the Balance table in the database
+ */
 @Repository
 public interface BalanceRepository extends JpaRepository<Balance, BigInteger> { }
 
