e34a5c92be8b03a9d9852dcc5f5a0daa2af709e7
Olagunju Babatunde Yunus <thundex001@gmail.com>
2018-11-27 20:20:46 +0000

    Add comments

--- src/main/java/com/example/transferService/repository/TransactionRepository.java
+++ src/main/java/com/example/transferService/repository/TransactionRepository.java
@@ -4,9 +4,14 @@ import com.example.transferService.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+/**
+ * Repository that interacts with the Transaction table in the database
+ */
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
-
+    /**
+     * Custom JPA query method to get the latest transaction in the database
+     * @return
+     */
     Transaction findFirstByOrderByTransactionDateDesc();
 }
